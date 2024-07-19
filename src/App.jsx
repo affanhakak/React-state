e4fff0ed77ca98df2c3d47e38d28cc6de47cddf3
Affan Hakak <affanshowkat@gmail.com>
2024-07-19 11:38:10 +0530

    initialising function for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./Counter";
+import Dumbo from "./Dumbo";
 
 function App() {
   return (
     <>
       <h1>React-State</h1>
       <Counter />
+      <Dumbo />
     </>
   );
 }
---
+++ src/Dumbo.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function generateGameBoard() {
+  alert("state changed");
+  return Array(5000);
+}
+
+export default function Dumbo() {
+  const [board, setBoard] = useState(generateGameBoard);
+  const changeState = () => {
+    setBoard("hello");
+  };
+  return (
+    <>
+      <button onClick={changeState}>Click to change state</button>
+    </>
+  );
+}
+
+//initializer function what can be passed to useState initially
